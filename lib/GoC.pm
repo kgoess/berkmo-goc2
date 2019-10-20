91d38282ed7ed2e29b70b88bdf8e08c4ba0c0cb3
Kevin M. Goess <kevin@goess.org>
2019-10-19 19:53:37 -0700

    Add skeletal GoC.pm
    for the Makefile.PL

---
+++ lib/GoC.pm
@@ -0,0 +1,42 @@
+package GoC;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+__END__
+
+=head1 NAME
+
+GoC - The Grid of Committment
+
+=head1 SYNOPSIS
+
+See these files for usage instructions:
+
+lib/GoC/Controller/CGI.pm
+
+lib/GoC/Controller/ModPerl.pm
+
+=head1 DESCRIPTION
+
+This is the Grid of Committment.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Kevin M. Goess
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2019 by Kevin M. Goess
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.18.4 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+
