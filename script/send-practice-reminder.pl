c83960b157522205eb2d5b441614911b7455c563
Kevin M. Goess <kevin@goess.org>
2025-02-23 17:47:21 -0800

    Add upcoming gigs/parties to weekly email

--- script/send-practice-reminder.pl
+++ script/send-practice-reminder.pl
@@ -7,8 +7,14 @@ use 5.16.0;
 use warnings;
 use utf8;
 
+$ENV{GOC_URI_BASE} = '/goc2.cgi';
+
 use Email::Stuffer;
 use Getopt::Long;
+
+use GoC::View;
+use GoC::Model::Event;
+use GoC::Utils qw/uri_escape/;
 
 my ($from, $to, $help);
 GetOptions(
@@ -21,7 +27,7 @@ if (!($from && $to) || $help) {
     my $usage = <<'EOL';
 
     usage: $0
-        --from    'alice <aliace@example.com>'
+        --from    'alice <alice@example.com>'
         --to      'bob <bob@example.com>'
 
         -h|--help this help
@@ -30,7 +36,7 @@ EOL
     exit 1;
 }
 
-my $html_part = <<EOL;
+my $html_part_template = <<EOL;
 <div>
 Hello everyone,
 </div>
@@ -59,11 +65,54 @@ and make everyone's inboxes easier to manage.
 </div>
 
 <div>
+<div>
+<b>Upcoming Gigs:</b>
+</div>
+[% PROCESS eventtable
+    events = gigs
+-%]
+<div>
+<b>Upcoming Parties:</b>
+</div>
+[% PROCESS eventtable
+    events = parties
+-%]
+[% BLOCK eventtable -%]
+    [% FOREACH event IN events -%]
+        <p>[% event.date_pretty  | html -%]
+        <a href="[% uri_for(path => '/event', id => event.id) %]">
+        [% event.name | html -%]
+        </a>
+        <br>
+        Confirmed: [% event.get_num_persons('role', 'dancer', 'status', 'y') || 0 | html %] dancers / [% event.get_num_persons('role', 'muso', 'status', 'y') || 0 | html %] musos<br>
+        Not Coming: [% event.get_num_persons('role', 'dancer', 'status', 'n') || 0 | html %] dancers / [% event.get_num_persons('role', 'muso', 'status', 'n') || 0 | html %] musos<br>
+        Unsure: [% event.get_num_persons('role', 'dancer', 'status', '?') || 0 | html %] dancers / [% event.get_num_persons('role', 'muso', 'status', '?') || 0 | html %] musos<br>
+        [% IF ! event.count_is_ok && event.get_days_until_go_nogo -%]
+            [% event.get_days_until_go_nogo %] days until go/no-go<br>
+        [% END -%]
+    [% END -%]
+[% END -%]
+</div>
+
+<div>
 Cheers,<br>
 Your Friendly Neighborhood Practice Reminder Bot
 </div>
 
 EOL
+
+
+my $tt = Template->new();
+
+my $html_part;
+my %vars = (
+    uri_for        => \&uri_for,
+    gigs    => GoC::Model::Event->get_upcoming_events(type => 'gig'),
+    parties => GoC::Model::Event->get_upcoming_events(type => 'party'),
+
+);
+$tt->process(\$html_part_template, \%vars, \$html_part)
+    or die $tt->error;
 
 my $sendmail_to = $to;
 if ($sendmail_to =~ /<(.+)?>/) {
@@ -80,6 +129,14 @@ my $stuffer = Email::Stuffer
     ->html_body($html_part)
 ;
 
+#open my $logfh, ">", "/home/lucybear/berkeleymorris.org/kgtesting.html";
+#say $logfh '<html>';
+#say $logfh scalar localtime;
+#say $logfh $stuffer->as_string;
+#say $logfh '</html>';
+#close $logfh;
+#exit;
+
 open my $fh, '|-', "/usr/sbin/sendmail -i $sendmail_to"
     or die "can't pipe to mail $!";
 
@@ -88,3 +145,25 @@ print $fh $stuffer->as_string;
 close $fh or die "can't write to mail $!";
 
 
+# copied from GoC::Controller::ModPerl because moving it to a shared spot is
+# more than I have time for ATM
+sub uri_for {
+    my %p;
+    if (ref $_[0] eq 'HASH') { # TT sends a hashref
+        %p = %{ $_[0] };
+    } else {
+        %p = @_;
+    }
+
+    my $path = delete $p{path} || '/';
+
+    my $base = $ENV{GOC_URI_BASE} or die "GOC_URI_BASE is unset in ENV";
+
+    my $url_params = '';
+    if (keys %p) {
+        $url_params = '&'; # will also be different for mod_perl
+        $url_params .= join '&', map { "$_=".uri_escape($p{$_}) } sort keys %p;
+    }
+
+    return "$base?path=$path$url_params";
+}
